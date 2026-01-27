07468c7b1aaa47129b2b507fff7cf11b2469e799
elise211 <elisechiu211@gmail.com>
2026-01-27 14:49:22 +0800

    Add files via upload

---
+++ ServiceWorker01262.js
@@ -0,0 +1,33 @@
+const cacheName = "Stemtitute-Kindergarten-0.7.9";
+const contentToCache = [
+    "Build/Web01262.loader.js",
+    "Build/Web01262.framework.js",
+    "Build/Web01262.data",
+    "Build/Web01262.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
